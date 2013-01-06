7e85a4664b49cbe1fc9abdaa06b999baa3f57b8a
Dan Revel <drevel@gilt.com>
2013-01-05 21:29:58 -0800

    ajax

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :url "http://change.nopolabs.com"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.1.3"]
+                 [com.twinql.clojure/clj-conneg "1.1.0"]
+                 [org.clojure/data.json "0.2.0"]
                  [org.clojure/math.numeric-tower "0.0.2"]]
   :plugins [[lein-ring "0.7.5"]]
   :ring {:handler change.handler/app}
--- src/change/handler.clj
+++ src/change/handler.clj
@@ -5,12 +5,25 @@
   (:use change.view)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
-            [ring.util.response :as resp]))
+            [ring.util.response :as resp]
+            [clojure.data.json :as json]
+            [com.twinql.clojure.conneg :as conneg]))
+
+(defn json-response [data & [status]]
+  {:status (or status 200)
+   :headers {"Content-Type" "application/json"}
+   :body (json/write-str data)})
 
 (defroutes app-routes
   (GET "/" [] (resp/resource-response "index.html" {:root "public"}))
-  (GET ["/:amt", :amt #"[0-9]+"] [amt]
-       (view-change (make-lcm-breadth-first-change (Long. amt))))
+  (GET ["/:amt", :amt #"[0-9]+"] { params :params headers :headers }
+    (let [amt (Long. (:amt params))
+          accept-header (get headers "accept")
+          content-type (conneg/best-allowed-content-type accept-header #{"application/json" "text/html"})
+          change (make-lcm-breadth-first-change amt)]
+      (if (= ["application" "json"] content-type)
+        (json-response change)
+        (view-change change))))
   (route/resources "/")
   (route/not-found "Not Found"))
 
